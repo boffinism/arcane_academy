0a60a7b0e42a249deb368ac44839d95f58051b23
Patrick <patrick.c.gleeson@googlemail.com>
2016-05-30 19:40:52 +0100

    Create Levels submodule

--- logic_engine/level.rb
+++ logic_engine/level.rb
@@ -1,7 +1,7 @@
 module LogicEngine
   class Level
     def self.retrieve(level_number = 1)
-      LogicEngine.const_get("Level#{level_number}").new
+      LogicEngine::Levels.const_get("Level#{level_number}").new
     end
   end
 end
--- spec/unit/logic_engine/level_spec.rb
+++ spec/unit/logic_engine/level_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe LogicEngine::Level do
   describe '.retrieve' do
     it 'loads a level according to the number passed in' do
-      stub_const('LogicEngine::Level17', Class.new)
-      expect(LogicEngine::Level17).to receive(:new).and_return :level17
+      stub_const('LogicEngine::Levels::Level17', Class.new)
+      expect(LogicEngine::Levels::Level17).to receive(:new).and_return :level17
       expect(LogicEngine::Level.retrieve(17)).to eq :level17
     end
   end
